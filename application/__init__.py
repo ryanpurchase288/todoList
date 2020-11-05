3ee31a69f5991b73ef173549473c48b2449cd259
ryanpurchase288 <ryanpurchase288.rp@gmail.com>
2020-11-05 16:18:20 +0000

    added enviormental db uri and secret key

--- application/__init__.py
+++ application/__init__.py
@@ -1,10 +1,10 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-
+from os import getenv
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://root:devops@35.246.67.5/flaskDemo"
-app.config['SECRET_KEY'] = 'hndsnbaiohewdiowahnk'
+app.config['SQLALCHEMY_DATABASE_URI'] = getenv('DB_URI')
+app.config['SECRET_KEY'] = getenv('SECRET_KEY')
 db = SQLAlchemy(app)
 
 from application import routes
